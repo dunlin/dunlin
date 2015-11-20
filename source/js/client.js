021ef43e927ee89eab28dbf88ba3aa4e2fac707f
Christian Mücke <c.muecke@epages.com>
2015-11-20 14:37:51 +0100

    Design tweaks
    - add intro & idle animations
    - improve avatar hover effect
    - replace manual layout switch with media queries
    - move modules into separate folder
    - push more variables & mixins
    - improve property order

--- source/js/client.js
+++ source/js/client.js
@@ -1,5 +1,4 @@
 var $ = require('jquery'),
     webrtc = require('./rtc')(),
     avatar = require('./avatar')($('#avatar'), webrtc),
-    orientation = require('./orientation')($('#orientation-switch')),
     idle = require('./idle')(document.body);
